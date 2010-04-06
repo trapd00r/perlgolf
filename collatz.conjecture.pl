5ff10699625a570b16a3046f6c3259d9668618b9
trapd00r <trapd00r@trapd00r.se>
2010-04-06 07:31:05 +0200

    fail

--- collatz.conjecture.pl
+++ collatz.conjecture.pl
@@ -1,15 +1,15 @@
 #!/usr/bin/perl
 # Collatz conjecture
 use strict;
-my $int = shift or die;
+my $int = shift or die 'I need int';
+my $i = 0;
 while($int > 1) {
-  $int = $int/2 if $int % 2 == 0;
+  $i++;
   if($int %2 == 0) {
     $int = $int/2;
   }
   else {
     $int = $int*3+1;
   }
-  print $int, "\n";
-  sleep 1;
+  printf "%002s: %s\n", $i, $int;
 }
